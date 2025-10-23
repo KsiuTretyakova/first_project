857042e917781f23f509e8d865e63764a6bfda58
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-10-23 16:28:35 +0300

    func sum ex 8

--- string.py
+++ string.py
@@ -14,7 +14,7 @@ print(f"Hello, {name}! How are you?{n}")
 ex = input("Enter ex:\n")  # 5-3+1
 # len_ex = len(ex)  # 5
 
-n = (len(ex) + 1) // 2  # кількість цифр
+# n = (len(ex) + 1) // 2  # кількість цифр
 
 # 1 var
 # print(int(ex[0]) + (1 if ex[1] == "+" else -1) * int(ex[2]) + (1 if ex[3] == "+" else -1) * int(ex[4]))
@@ -27,14 +27,30 @@ n = (len(ex) + 1) // 2  # кількість цифр
 # print(sum)
 
 # 3 var
-sum = int(ex[0])
-for i in range(1, len(ex)):
-    if ex[i] == "+":
-        sum += int(ex[i+1])
-    elif ex[i] == "-":
-        sum -= int(ex[i+1])
-print(sum)
+# sum = int(ex[0])
+# for i in range(1, len(ex)):
+#     if ex[i] == "+":
+#         sum += int(ex[i+1])
+#     elif ex[i] == "-":
+#         sum -= int(ex[i+1])
+# print(sum)
 
 # 4 var
-# ex.replace("-", "+")
+
+
+#--------------------------
+# my_list = ["1", "2", 4, 7]
+# for el in range(len(my_list)):
+#     my_list[el] = str(my_list[el])
+# print(my_list)
+
+# new_list = list(map(int, my_list))
+# print(new_list)
+#-----------------------------------------------
+
+ex = ex.replace("-", "+-")
+parts = ex.split("+")
+
 # map()
+sum_ex = sum(map(int, parts))
+print(sum_ex)
