2d8827347eb66d21e922e6ade8351e74ce4c0fce
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-10-23 11:31:44 +0300

    new exercise (8 - str)

--- string.py
+++ string.py
@@ -6,3 +6,35 @@ name = "Alisa"
 n = 7
 print(f"Hello, {name}! How are you?{n}")
 # "dfg {name}".format("Alisa")
+
+
+# ------------------------------
+# 8 task
+
+ex = input("Enter ex:\n")  # 5-3+1
+# len_ex = len(ex)  # 5
+
+n = (len(ex) + 1) // 2  # кількість цифр
+
+# 1 var
+# print(int(ex[0]) + (1 if ex[1] == "+" else -1) * int(ex[2]) + (1 if ex[3] == "+" else -1) * int(ex[4]))
+
+# 2 var
+# sum = int(ex[0])
+# for i in range (1, len(ex), 2):
+#     sum += (1 if ex[i] == "+" else -1) * int(ex[i+1])
+
+# print(sum)
+
+# 3 var
+sum = int(ex[0])
+for i in range(1, len(ex)):
+    if ex[i] == "+":
+        sum += int(ex[i+1])
+    elif ex[i] == "-":
+        sum -= int(ex[i+1])
+print(sum)
+
+# 4 var
+# ex.replace("-", "+")
+# map()
