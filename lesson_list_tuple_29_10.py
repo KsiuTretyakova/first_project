fa89231c7f84a0c0e4ec6eeedeace3f516568f3a
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-10-29 16:59:04 +0200

    lesson_29_10

---
+++ lesson_list_tuple_29_10.py
@@ -0,0 +1,57 @@
+# [1, "a", 2.0]
+# list()
+
+#1
+# value = "1 2 65 4 34 6 96 34 2 94 3"
+# value_list = list(map(int, value.split()))
+# print(value_list[len(value_list)//2:])
+
+#2
+# value = "1 2 65 4 34 6 96 34 2 94 3"
+# value_list = list(map(int, value.split()))
+# v_l2 = value_list[:-2]
+# v_l1 = value_list[-2:]
+
+# result = v_l1 + v_l2
+# print(result)
+
+# v_list = value_list[-2:]
+# print(v_list)
+# v_list.extend(v_l2)
+# print(v_list)
+
+# v_list.append(v_l2)
+# print(v_list)
+# v_list.append("try")
+# print(v_list)
+
+
+
+# list(map(int, list))
+
+#3
+# languages = "Ukrainian French Bulgarian Norwegian Latvian"
+# list_lang = languages.split()
+# list_lang.sort()
+# # print(list_lang)
+# print(" ".join(list_lang))
+
+
+
+# languages = "Ukrainian French Bulgarian Norwegian Latvian"
+# list_lang = languages.split()
+# print(" ".join(sorted(list_lang)))
+
+
+#4
+languages = "Ukrainian French Bulgarian Norwegian Latvian"
+list_lang = languages.split()
+list_lang.sort()
+# print(list_lang)
+print(" ".join(list_lang))
+
+
+
+# languages = "Ukrainian French Bulgarian Norwegian Latvian"
+# list_lang = languages.split()
+# print(" ".join(sorted(list_lang)))
